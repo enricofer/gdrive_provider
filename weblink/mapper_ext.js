fe46ff17bf0a8bae5b82ed35be443deb83c1ef86
Enrico Ferreguti <enricofer@gmail.com>
2019-09-26 13:15:48 +0200

    search in public layers feature

--- weblink/mapper_ext.js
+++ weblink/mapper_ext.js
@@ -4,7 +4,8 @@ Ext.require([
     'Ext.form.Panel',
     'Ext.panel.Panel',
     'Ext.Viewport',
-    'GeoExt.component.Map'
+    'GeoExt.component.Map',
+    //'Ext.ux.grid.feature.Searching'
 ]);
 
 
@@ -250,9 +251,17 @@ Ext.application({
             bodyPadding: 5
         });
 
+        var searching = {
+            ftype: 'searching',
+            minChars: 2,
+            mode: 'local'
+         };
+
         list_panel = Ext.create('Ext.grid.Panel', {
             title: 'public_layers',
             id: 'visible_layers',
+            features: [searching],
+            //requires: ['Sandbox.view.SearchTrigger'],
             store: Ext.data.StoreManager.lookup('content_store'),
             columns: [
                 { text: 'layer', dataIndex: 'layer', flex: 1 }
@@ -342,5 +351,53 @@ Ext.application({
                 group_panel
             ]
         });
+
+        Ext.define('Sandbox.view.SearchTrigger', {
+            extend: 'Ext.form.field.Text',
+            alias: 'widget.searchtrigger',
+            triggers:{
+                search: {
+                    cls: 'x-form-search-trigger',
+                    handler: function() {
+                        this.setFilter(this.up().dataIndex, this.getValue())
+                    }
+                },
+                clear: {
+                    cls: 'x-form-clear-trigger',
+                    handler: function() {
+                        this.setValue('')
+                        if(!this.autoSearch) this.setFilter(this.up().dataIndex, '')
+                    }
+                }
+            },
+            setFilter: function(filterId, value){
+                var store = this.up('grid').getStore();
+                if(value){
+                    store.removeFilter(filterId, false)
+                    var filter = {id: filterId, property: filterId, value: value};
+                    if(this.anyMatch) filter.anyMatch = this.anyMatch
+                    if(this.caseSensitive) filter.caseSensitive = this.caseSensitive
+                    if(this.exactMatch) filter.exactMatch = this.exactMatch
+                    if(this.operator) filter.operator = this.operator
+                    console.log(this.anyMatch, filter)
+                    store.addFilter(filter)
+                } else {
+                    store.filters.removeAtKey(filterId)
+                    store.reload()
+                }
+            },
+            listeners: {
+                render: function(){
+                    var me = this;
+                    me.ownerCt.on('resize', function(){
+                        me.setWidth(this.getEl().getWidth())
+                    })
+                },
+                change: function() {
+                    if(this.autoSearch) this.setFilter(this.up().dataIndex, this.getValue())
+                }
+            }
+        })
+
 }
 });
